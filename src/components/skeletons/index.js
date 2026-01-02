ab69620faed9bb397eadcb827d97bb06a3b4f2d8
Leonardo Quintero <lleo5301@gmail.com>
2026-01-02 15:20:54 -0500

    auto-claude: 1.6 - Create an index file that exports all skeleton com

---
+++ src/components/skeletons/index.js
@@ -0,0 +1,6 @@
+// Base skeleton components
+export { default as Skeleton } from './Skeleton'
+export { default as SkeletonText } from './SkeletonText'
+export { default as SkeletonCard } from './SkeletonCard'
+export { default as SkeletonTable } from './SkeletonTable'
+export { default as SkeletonAvatar } from './SkeletonAvatar'
